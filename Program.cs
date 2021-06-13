209d1c713c6f8a7c32779f7b9640a079959a807e
Darius Walker <55506065+darius466@users.noreply.github.com>
2021-06-13 16:57:36 -0400

    Game is working, un-tested for accuracy

--- Program.cs
+++ Program.cs
@@ -1,74 +1,129 @@
 ﻿using System;
 using System.Threading;
 
-namespace CGOL {
+namespace CGOL
+{
+    public class GameOfLife
+    {
 
-    public class GameOfLife {
+        public int X { get; }
+        public int Y { get; }
+        public int[,] CurGen { get; set; }
 
-            public static int X = 10;
-            public static int Y = 10;
+        public int[,] NexGen { get; set; }
 
-            public static int [ , ] CurGen = new int [X, Y];
-            public static int [ , ] NexGen = new int [X, Y];
+        public GameOfLife(int x, int y)
+        {
+            X = x;
+            Y = y;
+            CurGen = new int[X, Y];
+            NexGen = new int[X, Y];
 
-            public GameOfLife() {
-                var rand = new Random();           
-                for (int i = 0; i < X; i++) {
-                    for (int j = 0; j < Y; j++) {
-                    if (rand.Next(0, 10) < 50)
+
+            var rand = new Random();           
+            for (int i = 0; i < X; i++)
+            {
+                for (int j = 0; j < Y; j++)
+                {
+                    if (rand.Next(0, 100) < 50)
                         CurGen[i, j] = 0;
                     else
                         CurGen[i, j] = 1;
-                    }
+                }
+            }
+        }
+
+        public void Draw() {
+
+            string output = "";
+            for (int i = 0; i < X; i++)
+            {
+                for (int j =0; j< Y; j++)
+                {
+                    if (CurGen[i, j] == 1)
+                        output += "X";
+                    else
+                        output += "O";
+                }
+                output += "\n";
+            }
+            Console.WriteLine(output);
+        }
+
+        public int getAlive(int x, int y)
+        {
+
+            int aliveCount = 0;
+            for (int i = -1; i <= 1; i++)
+            {
+                for (int j = -1; j <= 1; j++)
+                {
+                    if (x + i < 0 || x + i >= X)   
+                        continue;
+                    if (y + j < 0 || y + j >= Y)   
+                        continue;
+                    if (x + i == x && y + j == y)     
+                        continue;
+
+                    aliveCount += CurGen[x + i, y + j];
                 }
             }
 
-            public int getAlive(int x, int y) {
-                int aliveCount = 0;
-                for (int i = -1; i <= 1; i++) {
-                    for (int j = -1; j <= 1; j++) {
-                        aliveCount += CurGen[x + i, y + j];
-                    }
-                }
-                aliveCount -= CurGen[x, y];
-                return aliveCount;
-            }
-
-            public void growNext() {
-                for (int x = 0; x < X; x++) {
-                    for (int y = 0; y < Y; y++) {
+            return aliveCount;
+        }
+  
+        public void growNext()
+        {
+            for (int x = 0; x < X; x++)
+            {
+                for (int y = 0; y < Y; y++)
+                {
                     int live = getAlive(x, y);
+
                     if (CurGen[x, y] == 1 && live < 2)
                         NexGen[x, y] = 0;
+
                     else if (CurGen[x, y] == 1 && live > 3)
                         NexGen[x, y] = 0;
+
                     else if (CurGen[x, y] == 0 && live == 3)
                         NexGen[x, y] = 1;
+
                     else
                         NexGen[x, y] = CurGen[x, y];
-                    }
-                }
-                for (int i = 0; i < X; i++) {
-                    for (int j = 0; j < Y; j++) {
-                        CurGen[i,j] = NexGen[i,j];
-                        Console.SetCursorPosition(0,0);
-                        if (CurGen[i,j] == 1)
-                            Console.Write("x");
-                        else
-                            Console.Write("-");
-                    }
+
                 }
             }
 
-            internal class Program {
-                static void Main(string [] args) {
-                    GameOfLife gof = new GameOfLife();
-                    while(true) {
-                        gof.growNext();
-                    }
+            for (int i = 0; i < X; i++)
+            {
+                for (int j = 0; j < Y; j++)
+                {
+                    CurGen[i, j] = NexGen[i, j];
                 }
             }
-
+        }
+        internal class Program
+        {
+            static void Main(string[] args)
+            {
+            GameOfLife gameOfLife = new GameOfLife(10,10);
+            GoLTest g = new GoLTest();
+            Console.SetCursorPosition(0, 0);
+            int runs = 10;
+            int i = 0;
+            while (i < runs)
+            {
+                //g.DrawTest();
+                //g.growNextTest();
+                gameOfLife.Draw();
+                gameOfLife.growNext();
+                Thread.Sleep(125);
+                i++;
+            }
+        }
+    }
+        
 
     }
 }
